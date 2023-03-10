e8d0102a1105d8972a5f4b2c2606cba6eb9fffd5
diegoleone42 <92556285+diegoleone42@users.noreply.github.com>
2023-03-10 15:19:48 -0500

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -4,10 +4,92 @@
  */
 package adts;
 
-/**
- *
- * @author diego.gutierrezf
- */
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Queue {
-    
+        String expresion;
+
+        class Nodo {
+            char info[];
+            Nodo sig;
+        }
+
+        private Nodo raiz,fondo;
+
+        Queue() {
+            raiz=null;
+            fondo=null;
+        }
+
+        public boolean Colavacia (){
+            return raiz == null;
+        }
+
+        void insertar (char[] info)
+        {
+            Nodo nuevo;
+            nuevo = new Nodo ();
+            nuevo.info = info;
+            nuevo.sig = null;
+            if (Colavacia()) {
+                raiz = nuevo;
+                fondo = nuevo;
+            } else {
+                fondo.sig = nuevo;
+                fondo = nuevo;
+            }
+        }
+
+        public String extraer () {
+
+            if (!Colavacia ())
+            {
+                char []informacion = raiz.info;
+                if (raiz == fondo){
+                    raiz = null;
+                    fondo = null;
+                } else {
+                    raiz = raiz.sig;
+                }
+            }
+      return "salida";
+        }
+
+        public void imprimir() {
+
+            Nodo reco=raiz;
+            System.out.println("Listado de todos los elementos de la cola.");
+            while (reco!=null) {
+                System.out.print(reco.info+ " ");
+                reco=reco.sig;
+            }
+            System.out.println();
+
+        }
+
+
+
+        public static void main(String[] ar) {
+
+            Scanner in = new Scanner(System.in);
+            String expresion= "";
+            System.out.print("Ingrese la expresión: ");
+            expresion = in.nextLine();
+
+
+            char[] arrCh = expresion.toCharArray();
+                String salida= Arrays.toString(arrCh);
+
+
+
+            Queue cola1=new Queue();
+            cola1.insertar(arrCh);
+            cola1.imprimir();
+
+
+            System.out.println("Extraemos uno de la cola:"+ cola1.extraer());
+            cola1.imprimir();
+
+        }
 }
