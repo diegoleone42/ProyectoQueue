c9a8d6d1c02efc9bea86c0088f6babd8e8f2dbee
diegoleone42 <92556285+diegoleone42@users.noreply.github.com>
2023-03-12 14:14:00 -0500

    Add files via upload

---
+++ InterfaceTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package adts;
+import adts.GenericResizingQueue;
+/**
+ *
+ * @author diego.gutierrezf
+ */
+public class InterfaceTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String test ="Helloe world";
+        GenericResizingQueue<String> queue;
+        queue= new GenericResizingQueue<>();
+        
+        for (int i = 0;i<test.length();i++){
+            char chr = test.charAt(i);
+            queue.enqueue(String.valueOf(chr));
+        }
+    }
+    
+}
